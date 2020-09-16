56fc1d66e27c043a21879c75a7e51841a6ada3ba
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-09-16 08:54:44 -0400

    connection complete

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,18 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
     host: 'localhost',
+    port: 3306,
     user: 'root',
     password: 'root1234',
     database: 'burgers_DB'
 });
 
-// Is this correct???
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
 module.exports = connection;
