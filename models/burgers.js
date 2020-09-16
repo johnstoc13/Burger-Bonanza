0288022e2922eb06841ea938cf5fa3b8d34a6773
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-09-16 10:00:22 -0400

    db functions complete

--- models/burgers.js
+++ models/burgers.js
@@ -1,5 +1,28 @@
-// Create code here that will call the ORM functions
-// using burger specific input for the ORM
+// Import the ORM to create functions that will interact with the database
+const orm = require("../config/orm");
 
+const burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
+};
 
-// Export when finished
+// Export the database functions for the controller
+module.exports = burger;
